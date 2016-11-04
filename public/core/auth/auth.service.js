b1cf4c9f8c4f17ff8e6991c9aeacf09a2aa8fe5d
caspian154 <srtelle@gmail.com>
2016-11-04 00:01:27 -0400

    Removed files and fix the auth service.

--- public/core/auth/auth.service.js
+++ public/core/auth/auth.service.js
@@ -2,7 +2,7 @@ angular.
   module('core.auth').
   factory('Auth', ['$resource',
     function($resource, email, password) {
-      return $resource('api/authenticate', {email: email, password: password}, {
+      return $resource('api/authenticate', {email: '@email', password: '@password'}, {
         query: {
           method: 'POST',
           params: { email: email, password: password }
